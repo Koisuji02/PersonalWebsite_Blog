8395bda3d551cf49789f9c76852c84f07334fe14
Koisuji02 <mat.dom1609job@gmail.com>
2024-03-09 14:40:28 +0100

    First commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\matdo\Desktop\SITO_MIO\SITO_WEB_BLOG\venv
+set VIRTUAL_ENV=C:\Users\matdo\Desktop\SITO_MIO\PersonalWebsite_Blog\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
